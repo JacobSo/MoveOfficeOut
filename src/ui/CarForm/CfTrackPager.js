654cf897515af6670bdeee89a0038ca97b84641f
shanda4869@gmail.com <shanda4869@gmail.com>
2018-05-02 15:02:37 +0800

    android commit

--- src/ui/CarForm/CfTrackPager.js
+++ src/ui/CarForm/CfTrackPager.js
@@ -15,13 +15,13 @@ import ApiService from '../../network/CfApiService';
 
 const Dimensions = require('Dimensions');
 let jsCode = `
-       // var formatData = JSON.parse(data);
+        var formatData = JSON.parse(data);
 
         var map = new BMap.Map("allmap");
-        var point = new BMap.Point(formatData.data[0].lng, formatData.data[0].lat);
+        var point = new BMap.Point(formatData[0].lng, formatData[0].lat);
         map.centerAndZoom(point, 10);
         var pois = [];
-        formatData.data.map(function (temp) {
+        formatData.map(function (temp) {
             pois.push(new BMap.Point(temp.lng, temp.lat))
         });
 
@@ -75,7 +75,7 @@ export default class CfTrackPager extends Component {
         this.setState({isRefreshing: true});
         ApiService.getGpsHistory("20007378639110152522677532f63c4056f34968ef4598a97735e9912b0000010018010").then((responseJson) => {
             //this.refs.webView.postMessage(JSON.stringify(responseJson.data));
-            let temp  = 'var formatData ='+ JSON.stringify(responseJson.data)+';'
+            let temp  = 'var data =\''+ JSON.stringify(responseJson.data)+'\';'
             jsCode = temp +jsCode
             console.log(jsCode)
             this.setState({
